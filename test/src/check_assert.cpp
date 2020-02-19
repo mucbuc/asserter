db714b851a55bf6da048486fd8158754d1830bcb
mucbuc <mbusenitz@gmail.com>
2020-02-19 06:46:18 -0800

    format code

--- test/src/check_assert.cpp
+++ test/src/check_assert.cpp
@@ -1,14 +1,15 @@
+#include <exception>
 #include <string>
-#include <exception> 
 
 #include <tmp/src/test.h>
 
-int main(int argc, const char * argv[])
+int main(int argc, const char* argv[])
 {
-	using namespace std;
-	string a( "hello" ); 
+    using namespace std;
+    string a("hello");
 
-	ASSERT( !a.empty() )( a )( a.size() );
+    ASSERT(!a.empty())
+    (a)(a.size());
 
-	return 0; 
+    return 0;
 }
